69f46e789ec37a0fae284050e3a5e524f593b5bb
unknown <mca.21mmca18@silicon.ac.in>
2024-08-12 20:31:00 -0400

    Initial commit

---
+++ src/app/products/[productId]/page.tsx
@@ -0,0 +1,36 @@
+import { Metadata } from "next";
+
+type Props = {
+    params:{
+        productId: string;
+    };
+};
+export const generateMetadata =async ({params}:Props): Promise<Metadata> => {
+    const title = await new Promise(resolve => {
+        setTimeout(()=>{
+            resolve(`iPhone ${params.productId}`)
+        },100)
+    })
+    return {
+        // title: `Product ${params.productId}`,
+        title: `Product ${title}`,
+    }
+}
+
+
+
+export default function ProductDetails({params}:Props){
+    return <h1>Details about product {params.productId}</h1>;
+}
+
+
+
+
+
+
+
+// export default function ProductDetails({ params}:{
+//     params: {productId: string} //params is an object 
+// }){
+//     return <h1>Details about product {params.productId}</h1>;
+// }
---
+++ src/app/products/[productId]/reviews/[reviewsId]/page.tsx
@@ -0,0 +1,31 @@
+"use client"
+import { notFound } from "next/navigation";
+
+// 24.......
+function getRandomInt(count:number){
+    return Math.floor(Math.random() * count);
+}
+export default function ReviewDetail({params,}:{
+    params: {
+        productId : string ;
+        reviewsId : string ;
+    }
+}) {
+    // const random = getRandomInt(2);
+    // if(random === 1){
+    //     throw new Error("Error loading review");
+    // }
+    // .....................................................
+if(parseInt(params.reviewsId)>1000){
+    notFound();
+}
+
+return (
+    <h1>
+        Review {params.reviewsId} for product {params.productId}
+    </h1>
+)
+
+}
+
+    
